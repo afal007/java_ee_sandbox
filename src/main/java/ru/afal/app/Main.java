29c1b4287ea5724e284ebc8fe2724591040e37c6
afal <afal@diasoft.ru>
2017-12-21 15:40:27 +0700

    First classes

---
+++ src/main/java/ru/afal/app/Main.java
@@ -0,0 +1,4 @@
+package ru.afal.app;
+
+public class Main {
+}
---
+++ src/main/java/ru/afal/app/model/Customer.java
@@ -0,0 +1,41 @@
+package ru.afal.app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private BigDecimal balance;
+
+	public Customer() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId( Long id ) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName( String name ) {
+		this.name = name;
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance( BigDecimal balance ) {
+		this.balance = balance;
+	}
+}
