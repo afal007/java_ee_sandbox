2cb46cb03437fb2056844dfdf62b845b3a53c60f
afal <afal@diasoft.ru>
2018-02-12 13:08:41 +0700

    Some logic

--- business-logic/src/main/java/CustomerService.java
+++ business-logic/src/main/java/CustomerService.java
@@ -1,2 +1,17 @@
+import ru.afal.app.model.Customer;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+
 public class CustomerService {
+
+	@Inject
+	private EntityManager entityManager;
+
+	public Customer createCustomer( String name, String login, String password ) {
+		Customer newCustomer = new Customer( name, login, password, new BigDecimal( "0" ) );
+
+		return newCustomer;
+	}
 }
